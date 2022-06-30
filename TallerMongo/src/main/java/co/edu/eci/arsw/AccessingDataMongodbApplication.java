4c59724bc54631d1bb4871e1bb18270dc29fa603
unknown <juan.monroy-s@labinfo.is.escuelaing.edu.co>
2022-06-30 12:03:53 -0500

    añadir

---
+++ TallerMongo/src/main/java/co/edu/eci/arsw/AccessingDataMongodbApplication.java
@@ -0,0 +1,57 @@
+package co.edu.eci.arsw;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+public class AccessingDataMongodbApplication implements CommandLineRunner {
+
+    @Autowired
+    private CustomerRepository repository;
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(AccessingDataMongodbApplication.class);
+
+        Map<String, Object> propierties = new HashMap<>();
+        propierties.put("spring.data.mongodb.uri",
+                "mongodb+srv://dbAdmin:Clave123@cluster0.tnqcz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+
+        app.setDefaultProperties(propierties);
+        app.run(args);
+    }
+    public void run(String... args) throws Exception {
+
+        repository.deleteAll();
+
+        // save a couple of customers
+        repository.save(new Customer("Alice", "Smith"));
+        repository.save(new Customer("Bob", "Smith"));
+
+        // fetch all customers
+        System.out.println("Customers found with findAll():");
+        System.out.println("-------------------------------");
+        for (Customer customer : repository.findAll()) {
+            System.out.println(customer);
+
+        }
+        System.out.println();
+
+        // fetch an individual customer
+        System.out.println("Customer found with findByFirstName('Alice'):");
+        System.out.println("--------------------------------");
+        System.out.println(repository.findByFirstName("Alice"));
+
+        System.out.println("Customers found with findByLastName('Smith'):");
+        System.out.println("--------------------------------");
+        for (Customer customer : repository.findByLastName("Smith")) {
+            System.out.println(customer);
+        }
+
+    }
+
+}
---
+++ jdcprimerLive/src/main/java/co/edu/eci/arsw/AccessingDataJpaApplication.java
@@ -0,0 +1,4 @@
+package co.edu.eci.arsw;
+
+public class AccessingDataJpaApplication {
+}
